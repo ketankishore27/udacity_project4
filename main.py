a1b063e5bbbb7583c9e535a14b94da3c12b03da5
ketan.kishore@t-systems.com <ketan.kishore@t-systems.com>
2024-06-25 14:45:20 +0530

    added procfile

--- main.py
+++ main.py
@@ -116,4 +116,4 @@ async def inferences(user_data: User):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
